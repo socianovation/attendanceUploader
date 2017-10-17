0069166d7967b4efd274ed645f83a6ddd2d3e081
Yosia Sukiman <yosia@sleekr.id>
2017-10-17 16:34:01 +0700

    add function to read database and write the result

--- main.go
+++ main.go
@@ -1,46 +1,96 @@
 package main
 
 import (
-    "fmt"
-	"github.com/tealeg/xlsx"
-	"io/ioutil"
-	"strings"
+	"bufio"
 	"database/sql"
-	"github.com/go-sql-driver/mysql"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/denisenkom/go-mssqldb"
+)
+
+type AttendanceClock struct {
+	UserId    string `json:"user_id"`
+	Checktime string `json:"checktime"`
+	CompanyId string `json:"company_id"`
+}
+
+var (
+	debug         = flag.Bool("debug", false, "enable debugging")
+	password      = flag.String("password", "12345678", "the database password")
+	port     *int = flag.Int("port", 1433, "the database port")
+	server        = flag.String("server", "192.168.1.32", "the database server")
+	user          = flag.String("user", "fingerprint", "the database user")
+	database      = flag.String("d", "fingerprint", "fingerprint")
 )
 
 func main() {
-	b, err := ioutil.ReadFile("config.txt") // just pass the file name
-    if err != nil {
-        fmt.Print(err)
-    }
-	str := string(b) // convert content to a 'string'
-	strs := strings.Split(str, "\n")
-	
-	//Initialize DB
-	db, err := sql.Open("mysql", "root@127.0.0.1/dbname")
+	getConfig()
+	getData()
+}
+
+func getConfig() {
+
+}
+
+func getData() {
+	flag.Parse()
+
+	if *debug {
+		fmt.Printf(" password:%s\n", *password)
+		fmt.Printf(" port:%d\n", *port)
+		fmt.Printf(" server:%s\n", *server)
+		fmt.Printf(" user:%s\n", *user)
+	}
+
+	conn, err := sql.Open("mssql", "server=localhost;user id=fingerprint;password=12345678;port=1433;database=fingerprint")
 	if err != nil {
-		panic(err.Error())  // Just for example purpose. You should use proper error handling instead of panic
+		log.Fatal("Open connection failed:", err.Error())
 	}
-	defer db.Close()
-	// Prepare statement for reading data
-	stmtOut, err := db.Prepare("SELECT squareNumber FROM squarenum WHERE number = ?")
+	defer conn.Close()
+
+	rows, err := conn.Query("SELECT COUNT(USERID) FROM CHECKINOUT")
+	var count int
+	for rows.Next() {
+		err = rows.Scan(&count)
+	}
+
+	rows, err = conn.Query("SELECT USERID, CHECKTIME, sn FROM CHECKINOUT")
 	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
+		log.Fatal("Failed query:", err.Error())
 	}
-	defer stmtOut.Close()
-	
-	excelFileName := strs[0]
-    xlFile, err := xlsx.OpenFile(excelFileName)
-    if err != nil {
-		fmt.Printf("%s\n", err)
+
+	var userid string
+	var checktime string
+	var sn string
+	var attendanceClocks []AttendanceClock
+	f, err := os.Create("result.json")
+	w := bufio.NewWriter(f)
+	defer f.Close()
+
+	for rows.Next() {
+		rows.Scan(&userid, &checktime, &sn)
+		if userid == "" {
+			continue
+		}
+		attendanceClocks = append(attendanceClocks, AttendanceClock{userid, checktime, sn})
 	}
-    for _, sheet := range xlFile.Sheets {
-        for _, row := range sheet.Rows {
-            for _, cell := range row.Cells {
-                text := cell.String()
-                fmt.Printf("%s\n", text)
-            }
-        }
-    }
+
+	output, err := json.Marshal(attendanceClocks)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, string(output))
+
+	w.Flush()
+
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
 }
